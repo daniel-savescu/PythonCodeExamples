8879115775e248ccc84a3888efa1bc54b086ca29
Daniel Savescu <danielsavescu@Daniels-iMac.local>
2022-10-06 13:54:58 +0300

    binary to decima

---
+++ integerToBinary.py
@@ -0,0 +1,10 @@
+# Integer To Binary Converter
+
+try:    
+    numberToConvert = int(input("Enter a integer number to convert into binary: "))
+
+    print("The binary number is: ", bin(numberToConvert).replace("0b", " "))
+
+except ValueError:
+    
+    print("Enter only integers !")
